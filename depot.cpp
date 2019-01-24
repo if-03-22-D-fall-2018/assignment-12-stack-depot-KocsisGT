667a16513b67714fdddb0a8b47fbb33128bda378
KocsisGT <kocsispatrikistvan@gmail.com>
2019-01-24 13:34:22 +0100

    Commit

---
+++ depot.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "depot.h"
+
+Stack create_stack()
+{
+  return 0;
+}
---
+++ depot.h
@@ -0,0 +1,12 @@
+#ifndef ___DEPOT_H
+#define ___DEPOT_H
+#include "stack.h"
+
+struct Product
+{
+  int serial_no;
+};
+
+typedef struct DepotImplementation* Depot;
+
+#endif
